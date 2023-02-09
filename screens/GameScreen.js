f3eeb16ced8f09b99673a486adb9eaf58ffe507b
Nimna <avantha.nimna@gmail.com>
2023-02-09 18:51:13 +0530

    Game screan init

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,24 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-function GameScrean(){
-    return (<Text>
-        Game Screan
-    </Text>);
+function GameScrean() {
+  return (
+    <View style={styles.screan}>
+      <Text>Opponent's Guess</Text>
+      {/* Guess number */}
+      <View>
+        <Text>Higher or Lower</Text>
+        {/* +/- button */}
+      </View>
+      <View>{/* //log round */}</View>
+    </View>
+  );
 }
 
 export default GameScrean;
+
+const styles = StyleSheet.create({
+  screan: {
+    flex: 1,
+    padding: 12,
+  },
+});
