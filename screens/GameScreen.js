8946288210dee4ee06f4fb8cffc67126283e7c3a
Nimna <avantha.nimna@gmail.com>
2023-02-09 19:32:45 +0530

    create Titile in game screan

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 function GameScrean() {
   return (
     <View style={styles.screan}>
-      <Text>Opponent's Guess</Text>
+      <Text style={styles.titile}>Opponent's Guess</Text>
       {/* Guess number */}
       <View>
         <Text>Higher or Lower</Text>
@@ -20,5 +20,14 @@ const styles = StyleSheet.create({
   screan: {
     flex: 1,
     padding: 12,
+  },
+  titile: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#ddb52f",
+    textAlign: "center",
+    borderWidth: 2,
+    padding: 12,
+    borderColor: '#ddb52f'
   },
 });
