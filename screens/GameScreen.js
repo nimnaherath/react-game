ecaebf0f585344ddbfd13542798e9b9cf3df89e6
Nimna <avantha.nimna@gmail.com>
2023-02-10 10:47:25 +0530

    Generat eRandon number

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import Title from "../components/Title";
 
-function GameScrean() {
+function generateRandomNumber(min, max, exlude) {
+  const random = Math.floor(Math.random() * (max - min) + min);
+
+  if (random === exlude) {
+    return generateRandomNumber(max, min, exlude);
+  } else {
+    return random;
+  }
+}
+
+function GameScrean({ userNumber }) {
+  const initialGuess = generateRandomNumber(1, 100, userNumber);
+  const [surrentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screan}>
-        <Title>Opponent's Guess</Title>
+      <Title>Opponent's Guess</Title>
       {/* Guess number */}
       <View>
         <Text>Higher or Lower</Text>
