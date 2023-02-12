7a3ce480ac1a40b28e7e8a1aa61891c7bc64c592
Nimna <avantha.nimna@gmail.com>
2023-02-12 12:42:35 +0530

    Finish game over screan

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,21 +1,46 @@
-import { View, Image, Text, StyleSheet , Dimensions} from "react-native";
+import {
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  useWindowDimensions,
+} from "react-native";
 import Title from "../components/Title";
 import Colors from "../constants/colors";
 import PrimaryButton from "../components/PrimaryButton";
 
-function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
+function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 300;
+  }
+
+  if (height > 400) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: (imageSize/2),
+  };
+
   return (
     <View style={styles.rootContainer}>
       <Title>Game is Over</Title>
       <View style={styles.imageContainer}>
         <Image
-          style={styles.image}
+          style={[styles.image ,imageStyle]}
           source={require("../assets/images/success.png")}
         />
       </View>
       <View>
         <Text style={styles.summaryText}>
-          Your Phone needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess the number 
+          Your Phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{" "}
+          rounds to guess the number
           <Text style={styles.highlight}> {userNumber}</Text>.
         </Text>
       </View>
@@ -27,7 +52,7 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get('window').width;
+// const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   rootContainer: {
@@ -37,10 +62,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: deviceWidth < 300 ? 150: 300,
-    height: deviceWidth < 300 ? 150: 300,
-    borderRadius: deviceWidth < 300 ? 75: 150,
-    borderWidth: 3,
+    // width: deviceWidth < 300 ? 150: 300,
+    // height: deviceWidth < 300 ? 150: 300,
+    // borderRadius: deviceWidth < 300 ? 75: 150,
+    // borderWidth: 3,
     borderColor: Colors.primart800,
     overflow: "hidden",
     marginVertical: 36,
